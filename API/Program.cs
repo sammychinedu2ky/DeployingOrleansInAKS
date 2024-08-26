57fad0ced834947f7cb2e6aa9cfa97f82473d21b
Samson Amaugo <sammychinedu2ky@gmail.com>
2024-08-26 19:23:31 +0100

    pusher

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
-builder.AddServiceDefaults();
+//builder.AddServiceDefaults();
 var dashBoardPort = builder.Configuration["ORLEANS-SILO-DASHBOARD"]!;
 var redisConnectionString = builder.Configuration["ConnectionStrings:redis"]!;
 var dashboardPortInt = Convert.ToInt32(dashBoardPort);
@@ -8,27 +8,26 @@ var dashboardPortInt = Convert.ToInt32(dashBoardPort);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-//builder.AddKeyedRedisClient("redis");
-//builder.UseOrleans(option =>
-//{
-//    if(builder.Environment.IsProduction())
-//    {
-//        option.UseKubernetesHosting();
-//    }
-//    if (!String.IsNullOrEmpty(redisConnectionString))
-//    {
-//        Console.WriteLine($"Using Redis Clustering with connection string: {redisConnectionString}");
-//        option.UseRedisClustering(redisConnectionString);
+builder.AddKeyedRedisClient("redis");
+builder.UseOrleans(option =>
+{
+    if(builder.Environment.IsProduction())
+    {
+        option.UseKubernetesHosting();
+    }
+    //if (!String.IsNullOrEmpty(redisConnectionString))
+    //{
+    //option.UseRedisClustering(redisConnectionString);
 
-//    }
-//    option.UseDashboard(option =>
-//    {
-//        option.Port = dashboardPortInt;
-//    });
-//});
+    //}
+    option.UseDashboard(option =>
+    {
+        option.Port = dashboardPortInt;
+    });
+});
 var app = builder.Build();
 
-app.MapDefaultEndpoints();
+//app.MapDefaultEndpoints();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -39,12 +38,12 @@ if (app.Environment.IsDevelopment())
 
 
 
-//app.MapGet("/greet/{name}", (string name, IGrainFactory grainFactory) =>
-//{
-//    var helloGrain = grainFactory.GetGrain<IHelloGrain>(name);
-//    return helloGrain.SayHello(name);
-//})
-//.WithOpenApi();
+app.MapGet("/greet/{name}", (string name, IGrainFactory grainFactory) =>
+{
+    var helloGrain = grainFactory.GetGrain<IHelloGrain>(name);
+    return helloGrain.SayHello(name);
+})
+.WithOpenApi();
 
 app.MapGet("/", () => "Hello World");
 
